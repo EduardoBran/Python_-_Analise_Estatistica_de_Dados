dc9410890fff40707615af88d45159778bee14ff
EduardoBran <eduardo.ads1814@gmail.com>
2024-04-16 18:00:34 -0300

    resolvendo projeto com testes de hipoteses - parte 1

--- 05.Testes_de_Hipotese_Para_Problemas_de_Negocio_com_Telecom_em_R.R
+++ 05.Testes_de_Hipotese_Para_Problemas_de_Negocio_com_Telecom_em_R.R
@@ -81,6 +81,23 @@ library(randomForest)
 #     (Neste caso usaremos: Teste do Qui-Quadrado)
 
 
+#### Novas Perguntas:
+
+## 6) O uso do cartão melhorou significativamente em relação ao ano passado, que era 50?
+#     (Neste caso usaremos: )
+
+## 7) A última campanha foi bem-sucedida em termos de uso do cartão de crédito?
+#     (Neste caso usaremos: )
+
+## 8) Existe diferença de uso do cartão de crédito entre homens e mulheres?
+#     (Neste caso usaremos: )
+
+## 9) Existe diferença entre os segmentos de clientes em termos de uso do cartão de crédito?
+#     (Neste caso usaremos: )
+
+## 10) Existe uma relação entre o uso do cartão no último mês e o uso pré-campanha?
+#      (Neste caso usaremos: )
+
 
 
 #### Carregando os Dados
@@ -170,6 +187,23 @@ str(df)
 
 # -> Um valor-p de um teste t é a probabilidade de que os resultados de seus dados de amostra tenham ocorrido por acaso.
 
+# Executar o Teste t de Uma Amostra
+resultado_ttest <- t.test(df$consumo_medio_mes_anterior_ao_upgrade, mu = 50, alternative = "two.sided")
+resultado_ttest
+
+# Interpretação do valor-p
+if(resultado_ttest$p.value < 0.05) {
+  cat("Rejeitamos a hipótese nula (H0).\n
+      Portanto, o consumo médio de largura de banda antes do upgrade foi significativamente diferente de 50.\n")
+} else {
+  cat("Falhamos em rejeitar a hipótese nula (H0).\n
+      Portanto, não há evidência suficiente para afirmar que o consumo médio de largura de banda antes do upgrade foi diferente de 50.\n")
+}
+
+## Interpretando
+
+# - Rejeitamos a hipótese nula (H0).
+#   Portanto, o consumo médio de largura de banda antes do upgrade foi significativamente diferente de 50.
 
 
 
@@ -196,7 +230,180 @@ str(df)
 
 # -> Um valor-p de um teste t é a probabilidade de que os resultados de seus dados de amostra tenham ocorrido por acaso.
 
+# Executar o Teste t de Uma Amostra
+resultado_ttest <- t.test(df$consumo_medio_primeiro_mes_apos_upgrade, mu = 54, alternative = "two.sided")
+resultado_ttest
+
+# Interpretação do valor-p
+# Interpretação do valor-p
+if(resultado_ttest$p.value < 0.05) {
+  cat("Rejeitamos a hipótese nula (H0).\n",
+      "Portanto, o consumo médio de largura de banda no primeiro mês após o upgrade foi significativamente maior que 54.\n")
+} else {
+  cat("Falhamos em rejeitar a hipótese nula (H0).\n",
+      "Portanto, não há evidência suficiente para afirmar que o consumo médio de largura de banda no primeiro mês após o upgrade foi maior que 54.\n")
+}
+
+
+## Interpretando
+
+# - Falhamos em rejeitar a hipótese nula (H0). 
+#   Portanto, não há evidência suficiente para afirmar que o consumo médio de largura de banda no primeiro mês após o upgrade foi maior que 54.
+
+
+
+
 
 
 
 ## Pergunta 3:
+
+# - Houve diferença de consumo de largura de banda antes e depois do upgrade, considerando o primeiro mês após o upgrade?
+#   Neste caso usaremos: Teste t de Duas Amostras (Pareado)
+
+# Este teste é usado quando temos duas amostras que são relacionadas ou dependentes. Este é um teste para a hipótese nula de que duas amostras relacionadas 
+# têm valores médios (esperados) idênticos.
+
+# -> Quando usar: Este teste é utilizado quando as medições são feitas em pares correlacionados, ou seja, quando duas amostras estão relacionadas de alguma
+#                 maneira (antes e depois, por exemplo).
+
+# -> Por quê usar: O Teste t Pareado é útil para reduzir a variabilidade entre as amostras que poderia ser causada por variáveis não controladas, 
+#                  focando na diferença dentro dos pares. Isso é especialmente útil em estudos longitudinais ou estudos que envolvem medições repetidas.
+
+# -> Motivo da escolha: O Teste t Pareado foi escolhido porque é especificamente projetado para comparar duas amostras relacionadas, que neste caso são
+#                       as medidas de consumo de largura de banda antes e depois do upgrade para os mesmos clientes. Esse teste é ideal para avaliar a 
+#                       diferença significativa causada pelo upgrade, controlando as variáveis inerentes aos indivíduos que não mudam com o tempo.
+#                       Ele fornece uma análise direta e eficaz da mudança induzida pelo upgrade ao focar nas diferenças dentro dos mesmos sujeitos,
+#                       aumentando assim a precisão e a relevância dos resultados estatísticos para a pergunta de pesquisa.
+
+# Hipóteses:
+ 
+#  H0: O consumo antes do upgrade foi igual ao consumo após o upgrade (as médias são iguais).
+#  H1: O consumo antes do upgrade foi diferente do consumo após o upgrade (as médias não são iguais).
+
+# Se o valor-p for menor que 0,05 rejeitamos a H0. Caso contrário, falhamos em rejeitar a H0.
+
+
+
+
+## Interpretando
+
+# - 
+
+
+
+
+
+
+
+## Pergunta 4:
+
+# - O gênero do cliente influenciou o consumo de largura de banda no primeiro mês após o upgrade?
+#   Neste caso usaremos: Teste t de Duas Amostras Independentes
+
+# Calculamos o Teste t para as médias de duas amostras independentes. Este é um teste para a hipótese nula de que 2 amostras independentes têm valores
+# médios (esperados) idênticos. Este teste assume que as populações têm variâncias idênticas por padrão.
+
+# -> Quando usar: Este teste é aplicado quando queremos comparar as médias de duas amostras independentes de duas populações.
+
+# -> Por quê usar: Ele é útil quando duas amostras são coletadas de forma independente uma da outra e queremos avaliar se existe uma diferença
+#                  estatisticamente significativa entre suas médias.
+
+# -> Motivo da escolha: O Teste t de Duas Amostras Independentes foi escolhido para analisar se há diferenças no consumo de largura de banda entre gêneros
+#                       após um upgrade, porque permite comparar diretamente as médias de dois grupos independentes formados com base no gênero dos clientes. 
+#                       Este teste é particularmente adequado aqui, pois cada grupo (masculino e feminino) é tratado como uma amostra separada, sem 
+#                       dependência ou relação direta entre as observações dos dois grupos. Assim, o teste oferece um meio eficaz de verificar se as 
+#                       diferenças observadas no consumo médio de largura de banda são estatisticamente significativas entre homens e mulheres, ajudando a
+#                       determinar se o upgrade teve impactos diferenciados baseados no gênero.
+
+# Hipóteses:
+
+#  H0: Consumo de clientes do sexo masculino foi igual ao consumo de clientes do sexo feminino no primeiro mês após o upgrade
+#      (consumo médio foi igual entre os gêneros).
+
+#  H1: Consumo de clientes do sexo masculino não foi igual ao consumo de clientes do sexo feminino no primeiro mês após o upgrade
+#      (consumo médio não foi igual entre os gêneros).
+
+# Se o valor-p for menor que 0,05 rejeitamos a H0. Caso contrário, falhamos em rejeitar a H0.
+
+
+
+
+## Interpretando
+
+# - 
+
+
+
+
+
+
+## Pergunta 5:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 1) Teste t de Uma Amostra
+# Exemplo: Um pesquisador deseja verificar se a temperatura média em uma cidade é diferente de 25°C. Ele coleta a temperatura média diária durante um mês
+#          e aplica um Teste t de Uma Amostra.
+
+# Motivo da escolha: O Teste t de Uma Amostra é ideal aqui porque permite comparar a média de uma amostra (temperaturas médias diárias) com um valor
+#                    específico (25°C). Este teste é usado para determinar se a média da amostra é estatisticamente diferente do valor conhecido ou
+#                    hipotético.
+
+
+# 2) Teste t de Duas Amostras (Pareado)
+# Quando usar: Este teste é utilizado quando as medições são feitas em pares correlacionados, ou seja, quando duas amostras estão relacionadas de alguma maneira (antes e depois, por exemplo).
+# 
+# Por quê usar: O Teste t Pareado é útil para reduzir a variabilidade entre as amostras que poderia ser causada por variáveis não controladas, focando na diferença dentro dos pares. Isso é especialmente útil em estudos longitudinais ou estudos que envolvem medições repetidas.
+# 
+# Exemplo: Um exemplo clássico é medir o peso de um grupo de indivíduos antes e depois de um programa de dieta de um mês. O teste pode ajudar a determinar se houve uma mudança significativa no peso médio.
+# 
+# 3) Teste t de Duas Amostras Independentes
+# Quando usar: Este teste é aplicado quando queremos comparar as médias de duas amostras independentes de duas populações.
+# 
+# Por quê usar: Ele é útil quando duas amostras são coletadas de forma independente uma da outra e queremos avaliar se existe uma diferença estatisticamente significativa entre suas médias.
+# 
+# Exemplo: Por exemplo, comparar o desempenho médio de alunos de duas escolas diferentes em um teste padrão ou a eficácia de dois diferentes tipos de medicamentos em dois grupos de pacientes distintos.
+# 
+# 4) Teste do Qui-Quadrado
+# Quando usar: O teste do Qui-Quadrado é usado para avaliar se há uma associação significativa entre duas variáveis categóricas.
+# 
+# Por quê usar: É útil para determinar se as diferenças nas contagens ou frequências observadas em categorias são devidas ao acaso ou a uma associação real entre as variáveis.
+# 
+# Exemplo: Avaliar se o gênero (masculino, feminino) está associado à preferência por um novo produto. Outro exemplo seria testar se a escolha de curso universitário é independente do país de origem do estudante.
+# 
+# 5) Comparação de Proporção vs. Comparação de Média
+# Necessidade de Distinção: Sim, é crucial saber se estamos comparando proporções ou médias:
+#   
+#   Comparação de proporções: Usada quando estamos interessados em comparar as proporções (percentagens) entre grupos. Testes típicos incluem o teste z para proporções e o teste do Qui-Quadrado.
+# Comparação de médias: Usada quando queremos comparar médias numéricas entre grupos. Testes típicos incluem os testes t (uma amostra, duas amostras pareadas, duas amostras independentes).
